e7b82aef8fccf2a875c6fff490f4b45557af84f3
umerIqbal <162830133+UmerIqbalAi@users.noreply.github.com>
2024-07-21 22:46:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var userrName = "Umer Iqbal";
+var msg = "Hello ".concat(userrName, ", would you like to learn some TypeScript today?");
+console.log(msg);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let userrName :string = "Umer Iqbal"
+let msg : string = `Hello ${userrName}, would you like to learn some TypeScript today?`
+console.log(msg)
