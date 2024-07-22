8600201e0db067385dac22df7a9ef5adb14e6f44
Your Name <you@example.com>
2024-07-22 17:05:28 +0500

    Delete unnecessary files

--- main.js
+++
@@ -1,3 +0,0 @@
-var userrName = "Umer Iqbal";
-var msg = "Hello ".concat(userrName, ", would you like to learn some TypeScript today?");
-console.log(msg);
--- main.ts
+++
@@ -1,3 +0,0 @@
-let userrName :string = "Umer Iqbal"
-let msg : string = `Hello ${userrName}, would you like to learn some TypeScript today?`
-console.log(msg)
